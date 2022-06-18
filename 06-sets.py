7ee9ae1cb0f7229b7eafa562e9e5e2ab9b6d30fd
Viknesh <skviknesh@gmail.com>
2022-06-18 08:27:39 +0530

    Delete 06-sets.py

--- 06-sets.py
+++
@@ -1,9 +0,0 @@
-# Python program to illustrate a set
-
-# define a set and its elements
-myset = set(["Geeks", "For", "Geeks"])
-
-#as set doesn't have duplicate elements so, 1 geeks will not be printed
-print(myset)
-
-#Code submitted by Susobhan Akhuli
