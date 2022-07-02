c9a745e8fbafd9350e6cf67a86d3bc65c1f3e063
skviknesh <skviknesh@gmail.com>
2022-07-02 14:00:33 +0530

    Day-> 02-July-2022

--- python-scripts-store/09-functions-generators.py
+++ python-scripts-store/09-functions-generators.py
@@ -129,6 +129,21 @@ def fun():
 print(fun())
 
 
+# Another way of mentioning datatypes in function calls: ##############################################################
+def add(num1: int, num2: int) -> int:
+    """Add two numbers"""
+    num3 = num1 + num2
+
+    return num3
+
+
+# Driver code
+num1, num2 = 5, 15
+ans = add(num1, num2)
+print(f"The addition of {num1} and {num2} results {ans}.")
+type(num1), type(num2), type(ans)  # (<class 'int'>, <class 'int'>, <class 'int'>)
+
+
 # Generators - Yield Keyword ###########################################################################################
 # Example 1:
 def numberGenerator(n):
